9990ec48962ba330570373d427689458b30272a9
Melis Filiz Esenkaya <127357851+Filiz5@users.noreply.github.com>
2024-10-20 22:26:54 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,6 @@ yum update -y
 yum install ansible -y
 yum install git -y
 cd /home/ec2-user
-git clone https://github.com/BenguCambay/Jenkins-Project.git
+git clone https: //github.com/Filiz5/Jenkins-Project.git
 cd Jenkins-Project
 chmod 400 /home/ec2-user/Jenkins-Project/Meliskey.pem
